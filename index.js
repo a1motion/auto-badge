1ee1c3009e6f5e699a0a5f200e156fc4ff488f16
Connor Love <connor@dotconnor.com>
2019-06-03 19:52:36 -0400

    ref: update code style

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const titleRegex = /(\w*)(?:\((\w*)\))?: (.*)/
+const titleRegex = /(\w*)(?:\((\w*)\))?: (.*)/;
 
 module.exports = (app) => {
   app.on(
     [`pull_request.opened`, `pull_request.edited`, `pull_request.synchronize`],
     async (context) => {
-      const config = await context.config(`badge.yml`)
-      console.log(config)
+      const config = await context.config(`badge.yml`);
+      console.log(config);
       if (!config || config === null) {
-        return
+        return;
       }
       const {
         payload: {
           pull_request: { title, labels },
         },
-      } = context
+      } = context;
       if (titleRegex.test(title)) {
-        const [, type, scope, message] = titleRegex.exec(title)
-        let label = null
+        const [, type, scope, message] = titleRegex.exec(title);
+        let label = null;
         if (config && config.types[type]) {
           if (typeof config.types[type] === `string`) {
-            label = config.types[type]
+            label = config.types[type];
           } else if (typeof config.types[type] === `object`) {
             if (scope && config.types[type][scope]) {
-              label = config.types[type][scope]
+              label = config.types[type][scope];
             } else if (config.types[type].default) {
-              label = config.type[type].default
+              label = config.type[type].default;
             }
           }
         } else if (config.default) {
-          label = config.default
+          label = config.default;
         }
         if (label !== null) {
-          const oldLabels = labels.map((lab) => lab.name)
-          const newLabels = Array.from(new Set([...oldLabels, label]))
-          const params = context.issue({ labels: newLabels })
-          context.github.issues.addLabels(params)
+          const oldLabels = labels.map((lab) => lab.name);
+          const newLabels = Array.from(new Set([...oldLabels, label]));
+          const params = context.issue({ labels: newLabels });
+          context.github.issues.addLabels(params);
         }
       }
     }
-  )
-}
+  );
+};
