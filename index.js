ac7467fb92716bc154d7f2dbec66e7b2c07279d4
Connor Love <connor@dotconnor.com>
2019-06-29 04:29:59 -0400

    feat: add context logging

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = (app) => {
     [`pull_request.opened`, `pull_request.edited`, `pull_request.synchronize`],
     async (context) => {
       const config = await context.config(`badge.yml`);
-      console.log(config);
       if (!config || config === null) {
+        context.log(`No config found, skipping`);
         return;
       }
       const {
@@ -31,6 +31,7 @@ module.exports = (app) => {
           label = config.default;
         }
         if (label !== null) {
+          context.log(`Using label:`, label);
           const oldLabels = labels.map((lab) => lab.name);
           const newLabels = Array.from(new Set([...oldLabels, label]));
           const params = context.issue({ labels: newLabels });
